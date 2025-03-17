af172bffe65075bd719875dc210e05b24f970e42
arielglvz <arielglvz@gmail.com>
2025-03-17 08:00:09 +0800

    initial development

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import Container from "./ui/Container"
+import Footer from "./ui/Footer"
+import Header from "./ui/Header"
+
+function App() {
+
+  return (
+    <main>
+      <Container>
+        <p>Ecommerce demo</p>
+      </Container>
+    </main>
+  )
+}
+
+export default App
