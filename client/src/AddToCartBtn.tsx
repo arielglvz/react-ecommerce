160e0bffe912cbfd8df017a7f6e010a7abe4abce
arielglvz <arielglvz@gmail.com>
2025-03-19 03:53:31 +0800

    Feat: Added Pagination, ProductCard and Inital Development of AddToCartBtn component

---
+++ client/src/AddToCartBtn.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+const AddToCartBtn = ({ 
+  className, 
+  title = "Add to cart"
+}: {
+  className?: string;
+  title?: string
+}) => {
+  
+  const newClassName = twMerge('bg-[#f7f7f7] uppercase text-xs py-3 text-center rounded-full font-semibold hover:bg-black hover:text-white hover:scale-105 duration-200 cursor-pointer', className)
+  return (
+    <button className={newClassName}>
+     { title }
+    </button>
+  )
+}
+
+export default AddToCartBtn
