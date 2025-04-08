66099817f08bfaeff74c94861d64da3288f57d16
arielglvz <arielglvz@gmail.com>
2025-04-08 22:16:37 +0800

    FIX: Vercel error for client deployment

--- client/src/ui/AddToCartBtn.tsx
+++ client/src/ui/AddToCartBtn.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react'
-import { twMerge } from 'tailwind-merge'
-import { ProductProps } from '../type';
-import { store } from '../lib/store';
-import toast from 'react-hot-toast';
-import { FaMinus, FaPlus } from 'react-icons/fa';
-import PriceTag from './PriceTag';
-
+import { useEffect, useState } from "react"
+import { twMerge } from "tailwind-merge"
+import { ProductProps } from "../type";
+import { store } from "../lib/store";
+import toast from "react-hot-toast";
+import { FaMinus, FaPlus } from "react-icons/fa";
+import PriceTag from "./PriceTag";
 
 const AddToCartBtn = ({
   className,
@@ -42,12 +41,12 @@ const AddToCartBtn = ({
         decreaseQuantity(existingProduct?._id);
         toast.success(`${product?.name.substring(0,10)} decreased successfully!`);
       } else {
-        toast.error('You can not decrease less than 1');
+        toast.error("You can not decrease less than 1");
       }
     }
   }
 
-  const newClassName = twMerge('bg-[#f7f7f7] uppercase text-xs py-3 text-center rounded-full font-semibold hover:bg-black hover:text-white hover:scale-105 duration-200 cursor-pointer', className)
+  const newClassName = twMerge("bg-[#f7f7f7] uppercase text-xs py-3 text-center rounded-full font-semibold hover:bg-black hover:text-white hover:scale-105 duration-200 cursor-pointer", className)
   
   const getRegularPrice = () => {
     if (existingProduct) {
@@ -81,10 +80,10 @@ const AddToCartBtn = ({
         </div>
       )}
       { existingProduct 
-        ? <div className='flex flex-start items-center justify-center gap-2'>
-            <button onClick={handleDeleteProduct} className='bg-[#f7f7f7] text-black p-2 border-[1px] border-gray-200 hover:border-skyText rounded-full text hover:bg-white duration-200 cursor-pointer'><FaMinus/></button>
-            <p className='text-base font-semibold w-10 text-center'>{existingProduct?.quantity}</p>
-            <button onClick={handleAddToCart} className='bg-[#f7f7f7] text-black p-2 border-[1px] border-gray-200 hover:border-skyText rounded-full text-base hover:bg-white duration-200 cursor-pointer'><FaPlus/></button>
+        ? <div className="flex flex-start items-center justify-center gap-2">
+            <button onClick={handleDeleteProduct} className="bg-[#f7f7f7] text-black p-2 border-[1px] border-gray-200 hover:border-skyText rounded-full text hover:bg-white duration-200 cursor-pointer"><FaMinus/></button>
+            <p className="text-base font-semibold w-10 text-center">{existingProduct?.quantity}</p>
+            <button onClick={handleAddToCart} className="bg-[#f7f7f7] text-black p-2 border-[1px] border-gray-200 hover:border-skyText rounded-full text-base hover:bg-white duration-200 cursor-pointer"><FaPlus/></button>
           </div>
         : <button onClick={handleAddToCart} className={newClassName}>
         {title ? title : "Add to cart"}
