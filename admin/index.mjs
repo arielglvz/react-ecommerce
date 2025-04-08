b89fd46e16eb564f088517587134554343ad5927
arielglvz <arielglvz@gmail.com>
2025-04-08 20:39:56 +0800

    Deploying Backend in Vercel

--- admin/index.mjs
+++ admin/index.mjs
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import 'dotenv/config';
-import cors from 'cors';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import { readdirSync } from 'fs';
+import "dotenv/config";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
+import { readdirSync } from "fs";
 
 const port = process.env.PORT || 8000;
 app.use(cors());
@@ -17,13 +17,13 @@ const routesPath = path.resolve(__dirname, "./routes");
 const routeFiles = readdirSync(routesPath);
 routeFiles.map(async (file) => {
   const routeModule = await import(`./routes/${file}`);
-  app.use('/', routeModule.default);
-})
+  app.use("/", routeModule.default);
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));   
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));   
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`)
+  console.log(`Server is running on port: ${port}`);
 });
